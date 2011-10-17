884b34b696d824b8b5b139ab9b0503f0a00ec899
Peter Hosey <prh@Featherweight.(none)>
2011-10-17 15:52:38 -0700

    Initial Commit

---
+++ Programmer's Killswitch/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Programmer's Killswitch
+//
+//  Created by Peter Hosey on 2011-10-17.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
