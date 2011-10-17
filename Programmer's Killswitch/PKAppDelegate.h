5de6f859c57fcd63b613e683452d0556b3bdd74f
Peter Hosey <git@boredzo.org>
2011-10-17 15:52:38 -0700

    Initial Commit

---
+++ Programmer's Killswitch/PKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PKAppDelegate.h
+//  Programmer's Killswitch
+//
+//  Created by Peter Hosey on 2011-10-17.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PKAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Programmer's Killswitch/PKAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PKAppDelegate.m
+//  Programmer's Killswitch
+//
+//  Created by Peter Hosey on 2011-10-17.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PKAppDelegate.h"
+
+@implementation PKAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
